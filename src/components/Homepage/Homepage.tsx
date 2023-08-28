5bfb0658af714ff432ff921ec353b1d981b36ef0
mohi14 <mohiuddinkhan1409@gmail.com>
2023-08-28 16:57:03 +0600

    successfully converted to typescript

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,35 @@
+import "./Homepage.css";
+import Container from "react-bootstrap/Container";
+
+import TopMobileSearch from "./TopMobileSearch";
+import TopCategory from "./TopCategory";
+import VideoBlock from "./VideoBlock";
+import PopularChannels from "./PopularChannels";
+
+import FatFooter from "../Footer/FatFooter";
+import ContentWrapper from "../Atomics/ContentWrapper/ContentWrapper";
+
+const Homepage = () => {
+
+  const handleClick =():void=>{
+console.log("Oni chan clicked")
+  }
+  return (
+    <>
+      <ContentWrapper>
+        <Container fluid className="pb-0">
+          <TopMobileSearch />
+          <TopCategory />
+          <hr />         
+          <VideoBlock />
+          <hr className="mt-0" />
+          <PopularChannels />
+        </Container>
+
+        <FatFooter />
+      </ContentWrapper>
+    </>
+  );
+};
+
+export default Homepage;
---
+++ src/components/Sidebar/ListChannel.tsx
@@ -0,0 +1,82 @@
+// import { Link } from "react-router-dom";
+
+// const ListChannel = ({
+// 	href,
+// 	imgSrc,
+// 	imgAlt,
+// 	label,
+// 	labelBadge = null,
+// 	badgeValue = "",
+// }) => {
+// 	const labelVariant = `badge badge-${labelBadge}`;
+
+// 	const labelWithBadge = (
+// 		<>
+// 			{label} <span className={labelVariant}>{badgeValue}</span>
+// 		</>
+// 	);
+
+// 	return (
+// 		<>
+// 			<li>
+// 				<Link to={href}>
+// 					<img
+// 						className="img-fluid"
+// 						alt={imgAlt ? imgAlt : ""}
+// 						src={imgSrc}
+// 					/>{" "}
+// 					{labelBadge ? labelWithBadge : label}
+// 				</Link>
+// 			</li>
+// 		</>
+// 	);
+// };
+
+// export default ListChannel;
+
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface ListChannelProps {
+  href: string;
+  imgSrc: string;
+  imgAlt?: string;
+  label: string;
+  labelBadge?: string | null;
+  badgeValue?: string;
+}
+
+const ListChannel: React.FC<ListChannelProps> = ({
+  href,
+  imgSrc,
+  imgAlt,
+  label,
+  labelBadge = null,
+  badgeValue = "",
+}) => {
+  const labelVariant = `badge badge-${labelBadge}`;
+
+  const labelWithBadge = (
+    <>
+      {label} <span className={labelVariant}>{badgeValue}</span>
+    </>
+  );
+
+  return (
+    <>
+      <li>
+        <Link to={href}>
+          <img
+            className="img-fluid"
+            alt={imgAlt ? imgAlt : ""}
+            src={imgSrc}
+          />{" "}
+          {labelBadge ? labelWithBadge : label}
+        </Link>
+      </li>
+    </>
+  );
+};
+
+export default ListChannel;
